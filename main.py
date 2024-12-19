f1680b2a42cb355f4f8d139bb4af854cbf29b1a8
Miguel Tubac <mgtubac@gmail.com>
2024-12-18 21:51:37 -0600

    Se inicio con el proyecto 2

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.classes import Persona
+
+def main() -> None:
+    nuevaPersona = Persona.Persona("Miguel", 25)
+    #print(f"Nombre:  {nuevaPersona.get_nombre()}")
+    #print(f"Edad: {nuevaPersona.get_edad()}")
+    print(nuevaPersona)# Aca no me imprimira la direccion de memoria sino que el metodo __str__ de la clase persona
+
+if __name__ == '__main__':
+    main()
