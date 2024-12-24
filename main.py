ce1102c4cf1b714083a3458046123b410d187b78
Miguel Tubac <mgtubac@gmail.com>
2024-12-23 23:22:08 -0600

    Se agrego la parte del arbolb

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from src.classes.Persona import Persona
 from src.DataStructs.ArbolB.ArbolB import ArbolB
 
@@ -8,14 +9,26 @@ def main() -> None:
     print(nuevaPersona)# Aca no me imprimira la direccion de memoria sino que el metodo __str__ de la clase persona
 
     nuevoArbol: ArbolB = ArbolB(5)#Este es el valor de orden
-    nuevoArbol.insertar_clave(5)
-    nuevoArbol.insertar_clave(6)
-    nuevoArbol.insertar_clave(4)
-    nuevoArbol.insertar_clave(2)
-    nuevoArbol.insertar_clave(9)
-    nuevoArbol.insertar_clave(7)
+    nuevoArbol.insertar_valor(5)
+    nuevoArbol.insertar_valor(6)
+    nuevoArbol.insertar_valor(4)
+    nuevoArbol.insertar_valor(2)
+    nuevoArbol.insertar_valor(7)
+    nuevoArbol.insertar_valor(9)
+    nuevoArbol.insertar_valor(10)
+    nuevoArbol.insertar_valor(3)
+    nuevoArbol.insertar_valor(1)
+    nuevoArbol.insertar_valor(12)
+    nuevoArbol.insertar_valor(0)
+
+    nuevoArbol.insertar_valor(-1)
+    nuevoArbol.insertar_valor(13)
+    nuevoArbol.insertar_valor(20)
+    nuevoArbol.insertar_valor(23)
 
     print(nuevoArbol.imprimir_usuario())
+
+    #os.system("dot -Tpng Reportes/arbol_b.dot -o Reportes/arbol_b.png")
 
 if __name__ == '__main__':
     main()
--- src/DataStructs/ArbolB/ArbolB.py
+++ src/DataStructs/ArbolB/ArbolB.py
@@ -1,98 +1,106 @@
+import math
 from src.DataStructs.ArbolB.NodoArbolB import NodoArbolB
 
 class ArbolB:
     def __init__(self, orden:int):
-        self.root: NodoArbolB = NodoArbolB(True)
+        self.raiz: NodoArbolB = NodoArbolB(True)
         self.orden:int = orden
 
-
     
-    def insertar_clave(self, valor:int):
-        root: NodoArbolB = self.root
+    def insertar_valor(self, valor:int):
+        raiz: NodoArbolB = self.raiz
 
-        if len(root.claves) == self.orden -1:
-            # aca va la separacion de la hoja
-            nodo:NodoArbolB = NodoArbolB()#al no pasarle los parametros se indica que no es una hoja
-            self.root = nodo
-            nodo.hijos.insert(0, root)
-            self.dividir_hijo(nodo, 0)
-            self.insertar_valor_no_completo(nodo, valor)
-        else:
-            # aca se inserta el valor
-            self.insertar_valor_no_completo(root, valor)
-            
+        self.insertar_valor_no_completo(raiz, valor)
+        if len(raiz.claves) > self.orden - 1:
+            nodo: NodoArbolB = NodoArbolB()
+            self.raiz  = nodo
+            nodo.hijos.insert(0, raiz)
+            self.dividir_pagina(nodo, 0)
+
 
     def insertar_valor_no_completo(self, raiz:NodoArbolB, valor:int):
-        contador: int = len(raiz.claves) - 1
-
-        if(raiz.hoja):
+        posicion: int = len(raiz.claves) -1 #Esto nos sirve para saber donde se incerta la nueva clave
+        if (raiz.hoja):
             raiz.claves.append(None)
+            while posicion>= 0 and valor< raiz.claves[posicion]:
+                raiz.claves[posicion+1] = raiz.claves[posicion]
+                posicion -= 1
 
-            while contador >= 0 and valor < raiz.claves[contador]:
-                raiz.claves[contador + 1] = raiz.claves[contador] # aca desplazo el valor hacia la derecha porque es menor
-                contador -= 1
-            raiz.claves[contador + 1] = valor
+            raiz.claves[posicion +1] = valor
         else:
-            while contador >= 0 and valor < raiz.claves[contador]:
-                contador -= 1
+            while posicion >= 0 and valor < raiz.claves[posicion]:
+                posicion -= 1
 
-            contador +=1
-            if len(raiz.hijos[contador].claves) == self.orden -1:
-                #separar nodos
-                self.dividir_hijo(raiz, contador)
-                if valor > raiz.claves[contador]:
-                    contador += 1
+            posicion += 1
+            self.insertar_valor_no_completo(raiz.hijos[posicion], valor) #esta es la llamada recursiva
+            if len(raiz.hijos[posicion].claves) > self.orden - 1:
+                self.dividir_pagina(raiz, posicion)
 
-            
-            self.insertar_valor_no_completo(raiz.hijos[contador], valor)
 
         
-    def dividir_hijo(self, raiz:NodoArbolB, posicion:int):
+    def dividir_pagina(self, raiz:NodoArbolB, posicion:int):
         posicion_media: int = int((self.orden -1)/2)
-        hijo:NodoArbolB = raiz.hijos[posicion]
-        nodo:NodoArbolB = NodoArbolB(hijo.hoja)
+
+        hijo: NodoArbolB = raiz.hijos[posicion]
+        nodo: NodoArbolB = NodoArbolB(hijo.hoja)
 
         raiz.hijos.insert(posicion + 1, nodo)
 
+        #primero se dividen las claves de la raiz
         raiz.claves.insert(posicion, hijo.claves[posicion_media])
-        nodo.claves = hijo.claves[posicion_media +1: posicion_media*2 +1]
+        nodo.claves = hijo.claves[posicion_media + 1:posicion_media*2 +1]
         hijo.claves = hijo.claves[0:posicion_media]
-
+        
         if not hijo.hoja:
-            nodo.hijos = hijo.hijos[posicion_media + 1:posicion_media * 2 +1]
-            hijo.hijos = hijo.hijos[0:posicion_media]
+            nodo.hijos = hijo.hijos[posicion_media + 1:posicion_media*2 + 2]
+            hijo.hijos = hijo.hijos[0:posicion_media +1]
 
+    
+    def imprimir_usuario(self) ->str:
+        dot: str = "diagrap G {"
+        #dot: str = 'digraph G {\n\tbgcolor="#1A1A1A";\n\t';
+        #dot += "fontcolor=white;\n\tnodesep=0.5;\n\tsplines=false\n\t";
+        #dot += 'node [shape=record width=1.2 style=filled fillcolor="#313638"';
+        #dot += "fontcolor=white color=transparent]; \n\t";
+        #dot += 'edge [fontcolor=white color="#0070C9"];\n\t';
 
-    # funcion para mostrar los la informacion de un objeto
-    def imprimir_usuario(self) -> str:
-        dot:str = "diagrap G { \n"
-        return self.imprimir(self.root)
+        dot += self.imprimir(self.raiz)
+
         dot += "\n}"
 
         return dot
 
-    def imprimir(self, nodo:NodoArbolB, id:int = 0) -> str:
+
+
+    # funcion para mostrar los la informacion de un objeto
+    def imprimir(self, nodo: NodoArbolB, id: list[int] = [0]) -> str:
         raiz:NodoArbolB = nodo
 
-        arbol:str = f"n{id}[label=\""
-        contador:int = 0
+        arbol = f'n{id[0]} [label="'
+        contador: int = 0;
         for item in raiz.claves:
-            if contador == len(raiz.claves) - 1:
-                arbol += f"<f{contador}>{item}"
+            if (contador == len(raiz.claves) - 1):
+                arbol += f"<f{contador}>|{item}|<f{contador + 1}>"
                 break
-            arbol += f"<f{contador}>{item}|"
+
+            arbol += f"<f{contador}>|{item} |"
+
             contador += 1
 
         arbol += "\"];\n\t"
-        contador:int = 0
-        id_padre = id
+
+        contador: int = 0
+        id_padre = id[0]
         for item in raiz.hijos:
-            arbol += f'n{id_padre}:f{contador} -> n{id +1};'
-            id += 1
+            '''if contador == len(raiz.hijos) -1:
+                    arbol += f'n{id_padre}:f{contador} -> n{id[0] + 1};\n\t'
+                else:
+            '''
+            arbol += f'n{id_padre}:f{contador} -> n{id[0] + 1};\n\t'
+            id[0] += 1
             arbol += self.imprimir(item, id)
 
             contador += 1
-
 
         return arbol
 
